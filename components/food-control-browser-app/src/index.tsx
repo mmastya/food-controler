82fbc16c4f6ad163391165b6bdeb042dbaf69b9e
Borodina <asya07200@gmail.com>
2019-11-24 13:18:00 +0300

    Установила основные зависимости. Инициализировала приложение.

--- components/food-control-browser-app/src/index.tsx
+++ components/food-control-browser-app/src/index.tsx
@@ -1,1 +1,45 @@
-console.log("Entry point for launch in browser");
+import { configure } from "mobx";
+import { App } from "pages/App/App";
+import React from "react";
+import ReactDOM from "react-dom";
+
+configure({
+  enforceActions: "always",
+});
+
+let container;
+
+const render = (Root): void => {
+  ReactDOM.render(<Root />, container);
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+  container = document.getElementById("root");
+
+  render(App);
+});
+
+declare let module: any;
+
+if (module.hot) {
+  module.hot.accept("./pages/App/App", () => {
+    render(App);
+  });
+}
+
+declare let process;
+
+if (process.env.NODE_ENV === "production") {
+  if ("serviceWorker" in navigator) {
+    window.addEventListener("load", () => {
+      navigator.serviceWorker
+        .register("/sw.js")
+        .then((registration) => {
+          console.log("SW registered: ", registration);
+        })
+        .catch((registrationError) => {
+          console.log("SW registration failed: ", registrationError);
+        });
+    });
+  }
+}
---
+++ components/food-control-browser-app/src/pages/App/App.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { Router, Switch, Route } from "react-router";
+import { createBrowserHistory } from "history";
+import { Home } from "../Home/Home";
+
+export const browserHistory = createBrowserHistory();
+
+export const App = (): JSX.Element => {
+  return (
+    <Router history={browserHistory}>
+      <Switch>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
+  );
+};
